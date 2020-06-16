87a863f3a1869ed6f52ebc92347afc5e50444aea
Martijn van Halderen <martijnmdesigns@hotmail.com>
2020-06-16 13:13:33 +0200

    Edited files to be more compatible and removed unecessary files

--- src/assets/ts/functions.ts
+++ src/assets/ts/functions.ts
@@ -1,3 +1,4 @@
+// Global function for truncating large amounts of text(paragraphs).
 export const truncateString = (string:string, num:number):string => {
     if (string.length <= (num)) {
         return string;
